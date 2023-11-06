fc561c3568ea132fdca3effceae11ad9b958dc11
Ashoka21 <118498180+Ashoka21@users.noreply.github.com>
2023-11-06 19:14:51 +0530

    Add files via upload

---
+++ delet/src/delet.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : delet.c
+ Author      : ashok
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(void) {
+	 /* prints !!!Hello World!!! */
+
+}
