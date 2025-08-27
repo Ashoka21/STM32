89696ceda8b01b9afd023292405d44588e5cb48d
Ashoka 21 <ashokm2112@gmail.com>
2025-08-27 15:50:58 +0530

    3 prog

---
+++ Documents/thinkpad backup/STUDY maTERIAL RMBD/import files/liux files/c++/cpp/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"enter the values of two digits\n";
+	cin>>a;
+	cin>>b;
+	sum=a+b;
+	cout<<"sum of two numkbers \n"<<sum;
+}
